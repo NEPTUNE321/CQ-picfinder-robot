a5bd49da9bd8d171fb11e6c88700de629e0a950d
NepNep <756316845@qq.com>
2019-07-17 23:21:43 +0800

    # test

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 require = require("esm")(module);
 module.exports = require("./main");
-module.exports = require("./app");
+
+const express = require('express');
+const app = express();
+
+//设置路由
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+//设置端口
+const server = app.listen(3000, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
