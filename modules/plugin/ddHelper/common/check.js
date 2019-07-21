374968ac4b61eeea7f2b5ea1a51683e369dfe435
NepNep <756316845@qq.com>
2019-07-21 22:38:07 +0800

    # 修改log

--- modules/plugin/ddHelper/common/check.js
+++ modules/plugin/ddHelper/common/check.js
@@ -9,13 +9,13 @@ const _user = require('./user')
 async function check (id) {
   let data = await _data(id)
   let user = await _user(id)
-  console.log(data)
   let info = {
     id: data.room_id,
     status: data.live_status === 1
   }
   if (info.status) {
     info.time = data.live_time
+    info.title = data.title
     info.name = user.info.uname
     info.unix = parseInt(new Date(data.live_time).getTime() / 1000)
   }
