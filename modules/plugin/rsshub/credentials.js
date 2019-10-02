0797102790efba4eacc55cacb38d5b44faa2ab70
NepNep <756316845@qq.com>
2019-10-03 03:04:32 +0800

    feat(env): addenv

--- modules/plugin/rsshub/credentials.js
+++ modules/plugin/rsshub/credentials.js
@@ -1,5 +1,6 @@
 module.exports = {
-  rsshub: 'http://localhost:1200',
+  // rsshub: 'http://localhost:1200',
+  rsshub: 'https://rsshub.app',
   // 有道翻译
   youdao: {
     appid: '38f0003e8af4ee2b',
